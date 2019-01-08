ccee44ef3a04aadfa2f14dfcaf41ec5250790373
Paweł Znamiec <auu788@gmail.com>
2019-01-08 21:47:52 +0100

    Add vuejs project with sample code

---
+++ frontend/src/components/OfferItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="offer-item">
+    <p>{{ offer.loading_place }} -> {{ offer.destination }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Offer Item",
+  props: ["offer"]
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ frontend/src/components/Offers.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="offers">
+    <h1>Oferty</h1>
+    <OfferItem v-for="offer in offers" :key="offer.id" v-bind:offer="offer"/>
+  </div>
+</template>
+
+<script>
+import OfferItem from "./OfferItem.vue";
+
+export default {
+  name: "Offers",
+  data: function() {
+    return {
+      offers: [
+        {
+          id: 1,
+          loading_place: "Łódź",
+          destination: "Warszawa"
+        },
+        {
+          id: 2,
+          loading_place: "Kielce",
+          destination: "Sieradz"
+        }
+      ]
+    };
+  },
+  components: {
+    OfferItem
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
